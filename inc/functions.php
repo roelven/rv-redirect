11f1bfafa6465bd9023d5320f82406d3eefae055
Roel van der Ven <roel@soundcloud.com>
2012-07-11 22:12:56 +0200

    delete ezSQL repo, remove gitmodule, cleanup

--- inc/functions.php
+++ inc/functions.php
@@ -1,9 +1,10 @@
 <?php
 
 // Add ezSQL DB wrapper
-include_once 'ezSQL/shared/ez_sql_core.php';
-include_once 'ezSQL/mysql/ez_sql_mysql.php';
+include_once 'ezSQL/ez_sql_core.php';
+include_once 'ezSQL/ez_sql_mysql.php';
 
+$host     = $_SERVER['SERVER_NAME'];
 $database = getenv('CLEARDB_DATABASE_URL');
 
 // XXX: I couldn't get getenv() to work on my mac environment, this is a workaround
@@ -11,11 +12,12 @@ if (!$database) {
   require_once('connect.php');
 }
 
-$url = parse_url($database);
+$url      = parse_url($database);
+$db       = new ezSQL_mysql($url['user'], $url['pass'], substr($url['path'], 1), $url['host']);
 
-$db = new ezSQL_mysql($url['user'], $url['pass'], substr($url['path'], 1), $url['host']);
+$send_to = $db->get_row("SELECT redirect_uri FROM domains WHERE host = ".$host);
 
-print_r($db);
+print $send_to->redirect_uri;
 
-
+$db->debug();
 ?>
