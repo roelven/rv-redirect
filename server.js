28f03e4066a0c1f2ce12ece94431142048bac3f2
Roel van der Ven <roel@soundcloud.com>
2012-07-12 15:31:03 +0200

    rewrote app for node.js

---
+++ server.js
@@ -0,0 +1,40 @@
+var http        = require('http'),
+    mysql       = require('mysql');
+
+http.createServer(function(request, response) {
+  var hostname    = require('url').parse('http://' + request.headers.host).hostname,
+      connection  = mysql.createConnection(process.env.CLEARDB_DATABASE_URL),
+      sql         = 'SELECT redirect_uri FROM domains WHERE host = ' + connection.escape(hostname);
+
+  connection.connect();
+
+  connection.query(sql, function(err, results) {
+    if (err) {
+      console.log(err);
+      response.writeHead(302, {
+        'X-Redirect-by': 'rv-redirect',
+        'Location': 'http://roelvanderven.com/rv-redirect-error'
+      });
+      response.end();
+    } else {
+      if (!results[0]['redirect_uri']) {
+        console.log('Domain unknown: ' + hostname + '. Sending home.');
+        response.writeHead(302, {
+          'X-Redirect-by': 'rv-redirect',
+          'Location': 'http://roelvanderven.com/rv-redirect'
+        });
+        response.end();
+      } else {
+        console.log('Redirecting ' + hostname + ' to ' + results[0]['redirect_uri']);
+        response.writeHead(302, {
+          'X-Redirect-by': 'rv-redirect',
+          'Location': 'http://' + results[0]['redirect_uri']
+        });
+        response.end();
+      }
+    };
+  });
+
+  connection.end();
+
+}).listen(8888);
